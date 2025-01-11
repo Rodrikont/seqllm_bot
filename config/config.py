95fdddb09ac48c2e49b48e69a94b81a67eac5761
Rodrikont <dan.savonin@gmail.com>
2025-01-11 13:06:51 +0300

    Some fixes 2

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ class Config:
         env.read_env()  # Загружает переменные из .env файла
 
         self.app_name = env.str("APP_NAME", "SEBot")
-        self.app_version = env.str("APP_VERSION", "0.1.1")
+        self.app_version = env.str("APP_VERSION", "v0.1.1")
         self.debug = env.bool("APP_DEBUG", False)
         self.tg_token = env.str("TELEGRAM_TOKEN", "7941346515:AAHFOqVTjrlIVGuR1BMEG9WEXXtj4YEGyWA")
         self.client_url = env.str("CLIENT_URL", "http://0.0.0.0:8080/")
